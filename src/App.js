2f4226e32c15ae0718555f01883b9dbcc3af700e
Jawad Bisharat <Jawad.bish@gmail.com>
2023-01-06 16:10:18 +0000

    style1

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ function App() {
   const [serviceRating, setServiceRating] = useState('');
   const [tipAmount, setTipAmount] = useState('');
 
-  
+
   function calculateTip(billAmount, serviceRating) {
     if (serviceRating < 5) {
-      return billAmount * 0.1;
+      return billAmount * 0.08;
     } else if (serviceRating < 8) {
-      return billAmount * 0.15;
+      return billAmount * 0.10;
     } else {
-      return billAmount * 0.2;
+      return billAmount * 0.15;
     }
   }
 
@@ -27,31 +27,39 @@ function App() {
   }
 
    return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Bill amount:
-        <input
-          type="number"
-          value={billAmount}
-          onChange={event => setBillAmount(event.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Service rating (1-10):
-        <input
-          type="number"
-          min="1"
-          max="10"
-          value={serviceRating}
-          onChange={event => setServiceRating(event.target.value)}
-        />
-      </label>
-      <br />
-      <button type="submit">Calculate tip</button>
-      {tipAmount && <p>Tip amount: {tipAmount}</p>}
-    </form>
-  );
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label className="form-label" htmlFor="bill-amount">
+            Bill amount:
+          </label>
+          <input
+            className="form-input"
+            id="bill-amount"
+            type="number"
+            value={billAmount}
+            onChange={event => setBillAmount(event.target.value)}
+          />
+        </div>
+        <div className="form-row">
+          <label className="form-label" htmlFor="service-rating">
+            Service rating (1-10):
+          </label>
+          <input
+            className="form-input"
+            id="service-rating"
+            type="number"
+            min="1"
+            max="10"
+            value={serviceRating}
+            onChange={event => setServiceRating(event.target.value)}
+          />
+        </div>
+        <button className="form-button" type="submit">
+          Calculate tip
+        </button>
+        {tipAmount && <p className="form-result">Tip amount: {tipAmount}</p>}
+      </form>
+    );
 }
 
 export default App;
